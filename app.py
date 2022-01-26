a72070cd8020526eba11cb7337ef48583fb30901
successor1 <kyildirim2002abc@gmail.com>
2022-01-26 18:46:42 +0100

    update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from flask import Flask, redirect, url_for, render_template, request, jsonify, send_from_directory
 from flask_restx import Resource, Api
-
 from audioop import add
 from doctest import master
 from email import header
@@ -11,7 +10,6 @@ from grpc import ServicerContext, StatusCode
 from pyqrllib.pyqrllib import str2bin, hstr2bin, bin2hstr
 from binascii import hexlify, a2b_base64
 from qrl.crypto.xmss import XMSS
-
 from qrl.core import config
 from qrl.core.AddressState import AddressState
 from qrl.core.OptimizedAddressState import OptimizedAddressState
@@ -43,6 +41,18 @@ from datetime import datetime
 from webargs import fields, validate
 from webargs.flaskparser import use_kwargs, parser, use_args
 import os
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+from  werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
+  
 
 logging.basicConfig(level=logging.INFO)
 
@@ -61,11 +71,86 @@ CONNECTION_TIMEOUT = 5
 
 HEX_SEED = environ.get('HEX_SEED')
 
-def valid_qaddress(request_form):
-    if len(request_form) == 79:
-        return True
-    else:
-        return False
+app.config["JWT_SECRET_KEY"] = environ.get('JWT_SECRET_KEY')
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+# database name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# creates SQLALCHEMY object
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+# Database ORMs
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    public_id = db.Column(db.String(50), unique = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(70), unique = True)
+    password = db.Column(db.String(80))
+  
+# decorator for verifying the JWT
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+  
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            print(data)
+            current_user = User.query\
+                .filter_by(public_id = data['public_id'])\
+                .first()
+        except:
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users contex to the routes
+        return  f(current_user, *args, **kwargs)
+  
+    return decorated
+
+
+parser_login = api.parser()
+parser_login.add_argument('email', type=str, required=True)
+parser_login.add_argument('password', type=int, required=True)
+
+# # signup route
+# @ns.route('/signup', methods =['POST'])
+# def signup():
+#     # creates a dictionary of the form data
+#     data = request.form
+  
+#     # gets name, email and password
+#     name, email = data.get('name'), data.get('email')
+#     password = data.get('password')
+  
+#     # checking for existing user
+#     user = User.query\
+#         .filter_by(email = email)\
+#         .first()
+#     if not user:
+#         # database ORM object
+#         user = User(
+#             public_id = str(uuid.uuid4()),
+#             name = name,
+#             email = email,
+#             password = generate_password_hash(password)
+#         )
+#         # insert user
+#         db.session.add(user)
+#         db.session.commit()
+  
+#         return make_response('Successfully registered.', 201)
+#     else:
+#         # returns 202 if user already exists
+#         return make_response('User already exists. Please Log in.', 202)
 
 def tx_unbase64(tx_json_str):
     tx_json = json.loads(tx_json_str)
@@ -85,7 +170,7 @@ def send_testnet_coins(addrs_to, amount, ots_key):
     bytes_addrs_to = []
     fee = 1000000000
     message_data = None
-    xmss_pk = XMSS.from_extended_seed(hstr2bin("010400bc2f62ec1161bba8eece8b511ce6e38f73254f42f045c08f403b9fb3f101a46d0f2b8d1b6da30c0ad8dd88b356e9022b")).pk
+    xmss_pk = XMSS.from_extended_seed(hstr2bin(HEX_SEED)).pk
     # if len([addrs_to]) > 1:
     #     for i in addrs_to:
     #         bytes_addrs_to.append(bytes(hstr2bin(i)))
@@ -133,6 +218,8 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 parser = api.parser()
+parser.add_argument('email', type=str, required=True)
+parser.add_argument('password', type=str, required=True)
 parser.add_argument('qaddress', type=str, required=True)
 parser.add_argument('amount', type=int, required=True)
 parser.add_argument('ots_key', type=int, required=True)
@@ -143,16 +230,59 @@ parser.add_argument('ots_key', type=int, required=True)
 """
 # Attention: 
 Return it to Q0104001f0b2c80a2b3843bc33e96c541801989d6a5dcbc17f0672de58853be808fcd24deed3470 once you're finished with the testnet coins!
+OTS key can be found on the explorer, search the above QRL address!
+Account registration can be done in Discord DMs
+
+Note: Your IP Address & email will be logged.
 """)
 class Faucet(Resource):
     @api.expect(parser, validate=True)
     def post(self):
+                # creates dictionary of form data
         args = parser.parse_args()
-        if valid_qaddress(args['qaddress']):
-            return send_testnet_coins(args['qaddress'], args['amount'], args['ots_key'])
-        else:
-            error = 'Invalid QRL address'
-            return error
+
+  
+        if not args['email'] or not args['password']:
+            # returns 401 if any email or / and password is missing
+            return make_response(
+                'Incorrect username or password!',
+                401,
+                {'WWW-Authenticate' : 'Basic realm ="Login required !!"'}
+            )
+  
+        user = User.query\
+            .filter_by(email = args['email'])\
+            .first()
+  
+        if not user:
+            # returns 401 if user does not exist
+            return make_response(
+                'Could not verify',
+                401,
+                {'WWW-Authenticate' : 'Basic realm ="User does not exist !!"'}
+            )
+  
+        if check_password_hash(user.password, args['password']):
+            # generates the JWT Token
+            # return make_response(jsonify({'token' : token}), 201)
+            if valid_qaddress(args['qaddress']):
+                ns.logger.info(dt_string + " | Faucet | email: " + str(args['email']) + " | amount: " + str(args['amount'])  + " | " + request.environ.get("HTTP_X_FORWARDED_FOR", request.remote_addr))
+                return send_testnet_coins(args['qaddress'], args['amount'], args['ots_key'])
+            else:
+                error = 'Invalid QRL address'
+                return error
+        # returns 403 if password is wrong
+        return make_response(
+            'Incorrect username or password!',
+            403,
+            {'WWW-Authenticate' : 'Basic realm ="Incorrect username or password!"'}
+        )
+
+def valid_qaddress(request_form):
+    if len(request_form) == 79:
+        return True
+    else:
+        return False
 
 @ns.route('/grpc/mainnet/GetHeight')
 class GetHeight_Mainnet(Resource):
